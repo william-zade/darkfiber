50b5b697bc996acc01b31554259c89075aebd880
William Zade <zadebill11@gmail.com>
2025-06-04 19:29:11 -0600

    Some more styling changes, but more importantly an updated README.md

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,11 @@ module.exports = {
     extend: {
       colors: {
         bg: "#0c0c0c",
-        green: "00ff00",
-        orange: "FF6F00",
+        green: "#00ff00",
+        orange: "#FF6F00",
         accent: "#1f1f1f",
         muted: "#8f8f8f",
-      }
+      },
       fontFamily: {
         sans: ["Inter", "sans-serif"],
         heading: ["Orbitron", "sans-serif"],
@@ -22,5 +22,3 @@ module.exports = {
   },
   plugins: [],
 };
-
-export default config;
